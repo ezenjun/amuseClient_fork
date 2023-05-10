ff3beeda01ac0c24a4c18ea4f105ac163abcfeab
inthhh <glass005533@gmail.com>
2023-05-10 20:52:45 +0900

    mainPage load title&price api

--- client/src/MainPage/MainBestItems/MainBestItem.tsx
+++ client/src/MainPage/MainBestItems/MainBestItem.tsx
@@ -3,16 +3,22 @@ import { useNavigate } from "react-router-dom";
 import Style from "../BestAndNewStyle.module.css";
 import axios from "axios";
 
+const numberWithCommas = (number: number): string => {
+  return number.toLocaleString("en");
+};
+
 interface BoxProps {
   marginRight: string;
   itemId: number;
   handleClick: () => void;
+  title: string;
+  startPrice: string;
 }
 
-const Box: React.FC<BoxProps> = ({ marginRight, itemId, handleClick }) => (
+const Box: React.FC<BoxProps> = ({ marginRight, itemId, handleClick, title, startPrice }) => (
   <div className={Style["box"]} style={{ marginRight }} onClick={handleClick}>
-    <p className={Style["tripTitle"]}>여행 제목</p>
-    <p className={Style["tripCost"]}>가격 : ~~~</p>
+    <p className={Style["tripTitle"]}>{title}</p>
+    <p className={Style["tripCost"]}>가격 : {startPrice}원 ~</p>
   </div>
 );
 
@@ -21,6 +27,8 @@ function MainBestItem() {
    * Best Item API
    */
   const [bestItemIds, setBestItemIds] = useState<number[]>([]);
+  const [bestItemTitle, setBestItemTitle] = useState([]);
+  const [bestItemPrice, setBestItemPrice] = useState<number[]>([]);
   useEffect(() => {
     axios
       .get("https://ammuse.store/main/best-item")
@@ -28,10 +36,13 @@ function MainBestItem() {
         const bestItems = response.data.data.bestItems;
         const ids = bestItems.map((item: any) => item.item_db_id);
         setBestItemIds(ids);
-
-        // console.log(response.data.data)
+        const titles = bestItems.map((item: any) => item.title);
+        setBestItemTitle(titles);
+        console.log(response.data.data);
+        const startPrices = bestItems.map((item: any) => item.startPrice);
+        setBestItemPrice(startPrices);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("연결 실패");
       });
   }, []);
@@ -48,11 +59,13 @@ function MainBestItem() {
     <div>
       <h2 style={{ marginTop: "1rem", marginBottom: "1rem" }}>실시간 Best 여행 상품🏞</h2>
       <div className={Style["container"]}>
-        {bestItemIds.map((itemId, index) => (
+        {bestItemIds.slice(0, 3).map((itemId: any, index: number) => (
           <Box
             key={itemId}
             marginRight={index === bestItemIds.length - 1 ? "0" : "32px"}
             itemId={itemId}
+            title={bestItemTitle[index]}
+            startPrice={numberWithCommas(bestItemPrice[index])}
             handleClick={() => navigateToDetail(itemId)}
           />
         ))}
--- client/src/MainPage/MainNewItems/MainNewItem.tsx
+++ client/src/MainPage/MainNewItems/MainNewItem.tsx
@@ -3,60 +3,72 @@ import { useNavigate } from "react-router-dom";
 import Style from "../BestAndNewStyle.module.css";
 import axios from "axios";
 
+const numberWithCommas = (num: number): string => {
+  return num.toLocaleString("en");
+};
+
 interface BoxProps {
   marginRight: string;
   itemId: number;
   handleClick: () => void;
+  title: string;
+  startPrice: string;
 }
 
-const Box: React.FC<BoxProps> = ({ marginRight, itemId, handleClick }) => (
+const Box: React.FC<BoxProps> = ({ marginRight, itemId, handleClick, title, startPrice }) => (
   <div className={Style["box"]} style={{ marginRight }} onClick={handleClick}>
-    <p className={Style["tripTitle"]}>여행 제목</p>
-    <p className={Style["tripCost"]}>가격 : ~~~</p>
+    <p className={Style["tripTitle"]}>{title}</p>
+    <p className={Style["tripCost"]}>가격 : {startPrice}원 ~</p>
   </div>
 );
 
 function MainNewItem() {
-   /**
+  /**
    * Current Item API
    */
-   const [currentItemIds, setCurrentItemIds] = useState<number[]>([]);
-   useEffect(() => {
+  const [currentItemIds, setCurrentItemIds] = useState<number[]>([]);
+  const [currentItemTitle, setCurrentItemTitle] = useState([]);
+  const [currentItemPrice, setCurrentItemPrice] = useState<number[]>([]);
+  useEffect(() => {
     axios
       .get("https://ammuse.store/main/current-item")
       .then((response) => {
         const currentItems = response.data.data.currentItems;
         const ids = currentItems.map((item: any) => item.item_db_id);
         setCurrentItemIds(ids);
-
-        // console.log(response.data.data.currentItems)
+        const titles = currentItems.map((item: any) => item.title);
+        setCurrentItemTitle(titles);
+        const startPrices = currentItems.map((item: any) => item.startPrice);
+        setCurrentItemPrice(startPrices);
+        console.log(response.data.data.currentItems);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("연결 실패");
       });
   }, []);
- 
-   /**
-    * Best Item -> Detail Page
-    */
-   const movePage = useNavigate();
-   const navigateToDetail = (itemId: number) => {
-     movePage(`/detail/${itemId}`);
-   };
- 
+
+  /**
+   * Best Item -> Detail Page
+   */
+  const movePage = useNavigate();
+  const navigateToDetail = (itemId: number) => {
+    movePage(`/detail/${itemId}`);
+  };
+
   return (
     <>
       <h2 style={{ marginTop: "3rem", marginBottom: "1rem" }}>어뮤즈의 최신 여행 패키지🚙</h2>
       <div className={Style["container"]}>
-        {currentItemIds.map((itemId, index) => (
+        {currentItemIds.slice(0, 3).map((itemId: any, index: number) => (
           <Box
             key={itemId}
             marginRight={index === currentItemIds.length - 1 ? "0" : "32px"}
             itemId={itemId}
+            title={currentItemTitle[index]}
+            startPrice={numberWithCommas(currentItemPrice[index])}
             handleClick={() => navigateToDetail(itemId)}
           />
         ))}
-
       </div>
     </>
   );
