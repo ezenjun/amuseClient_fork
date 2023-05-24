d34cb6f1885c0713f71878789f1fb680d100345e
inthhh <glass005533@gmail.com>
2023-05-24 13:41:07 +0900

    fix: Search Reset

--- client/src/Headers/Header.tsx
+++ client/src/Headers/Header.tsx
@@ -33,8 +33,8 @@ function Header() {
   };
 
   const navigateToSearch = () => {
-    window.location.reload();
     movePage(`/search/${searchKeyword}`);
+    window.location.reload();
   };
 
   // const handleSearch = () => {
