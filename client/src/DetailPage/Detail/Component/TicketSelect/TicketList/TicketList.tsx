8c46163752025d379d11a4ac6022c02254b8a470
rnjs010 <kyuri4809@naver.com>
2023-05-25 02:27:26 +0900

    add total amount

--- client/src/DetailPage/Detail/Component/TicketSelect/TicketList/TicketList.tsx
+++ client/src/DetailPage/Detail/Component/TicketSelect/TicketList/TicketList.tsx
@@ -17,7 +17,10 @@ function TicketList({ range, itemId }: DateProps) {
         title: string;
         content: string;
         priceList: { startDate: string; price: number; }[];
+        count: number;
     }
+
+
 
     const [ticketData, setTicketData] = useState<TicketData[]>([]);
 
@@ -26,57 +29,123 @@ function TicketList({ range, itemId }: DateProps) {
         axios
             .get(`https://ammuse.store/detail/${itemId}/ticket-select`)
             .then((response) => {
-                setTicketData(response.data.data.ticket)
+                const dataWithCount = response.data.data.ticket.map((ticket: TicketData) => ({
+                    ...ticket,
+                    count: 0,
+                }));
+                setTicketData(dataWithCount);
+                // setTicketData(response.data.data.ticket)
             })
             .catch(error => {
                 console.log("연결 실패");
             });
     }, []);
 
+    // plus button function
+    const handlePlus = (index: number) => {
+        setTicketData(prevData => {
+            const updatedData = [...prevData];
+            updatedData[index] = {
+                ...updatedData[index],
+                count: updatedData[index].count + 1,
+            }
+            return updatedData;
+        });
+    };
+
+    // minus button function
+    const handleMinus = (index: number) => {
+        setTicketData(prevData => {
+            const updatedData = [...prevData];
+            if (updatedData[index].count > 0) {
+                updatedData[index] = {
+                    ...updatedData[index],
+                    count: updatedData[index].count - 1,
+                }
+            }
+            return updatedData;
+        });
+    };
+
+    // date format change
+    function getSelectedPriceIndex(ticket: TicketData, range: DateRange | undefined):number {
+        return ticket.priceList.findIndex(priceItem => {
+            function getMonthNumber(monthString: string) {
+                const months = [
+                    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                ];
+                return months.findIndex(month => month === monthString);
+            }
+            const parts = priceItem.startDate.split(' ');
+            const month = getMonthNumber(parts[1]);
+            const day = parseInt(parts[2]);
+            const year = parseInt(parts[5]);
+            const date = new Date(year, month, day);
+
+            const formattedDate = format(date, 'yyyy-MM-dd');
+            return range?.from && isSameDay(parseISO(formattedDate), range.from)
+        });
+    }
 
     return (
         <div className='select-ticket'>
             <div className='TicketList'>
-                {/* 선택 날짜 출력*/}
+                {/*print selected date*/}
                 <p>{range?.from ? format(range.from, 'yyyy년 LL월 dd일') : ''}</p>
 
-                {ticketData.map((ticketInfo) => {
-                    const selectedPrice = ticketInfo.priceList.map(
-                        (priceItem) => {
-                            function getMonthNumber(monthString: string) {
-                                const months = [
-                                    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-                                ];
-                                return months.findIndex(month => month === monthString);
-                            }
-                            const parts = priceItem.startDate.split(' ');
-                            const month = getMonthNumber(parts[1]);
-                            const day = parseInt(parts[2]);
-                            const year = parseInt(parts[5]);
-                            const date = new Date(year, month, day);
-
-                            const formattedDate = format(date, 'yyyy-MM-dd');
-                            return range?.from && isSameDay(parseISO(formattedDate), range.from)
-                        }
-                    );
-
-                    const trueIndex = selectedPrice.indexOf(true);
-                    const price = trueIndex !== -1 ? ticketInfo.priceList[trueIndex].price : 0;
+                {/* ticket list */}
+                {ticketData.map((ticketInfo, index) => {
+                    const selectedPriceIndex = getSelectedPriceIndex(ticketInfo, range);
+                    const price = selectedPriceIndex !== -1 ? ticketInfo.priceList[selectedPriceIndex].price : 0;
                     return (
                         <Ticket
+                            key={index}
                             name={ticketInfo.title}
                             detail={ticketInfo.content}
                             price={price}
+                            count={ticketInfo.count}
+                            handleMinus={() => handleMinus(index)}
+                            handlePlus={() => handlePlus(index)}
                         />
                     );
                 })}
-                <div className='total-cost'>
 
-                </div>
+                {/* selected ticket price */}
+                {ticketData.filter(ticket => ticket.count > 0).map((ticket, index) => {
+                    const selectedPriceIndex = getSelectedPriceIndex(ticket, range);
+                    const price = selectedPriceIndex !== -1 ? ticket.priceList[selectedPriceIndex].price : 0;
+                    return (
+                        <div key={index} className='selected-ticket'>
+                            <div className='selected-ticket-title'>{ticket.title}</div>
+                            <div className='selected-ticket-count'>{ticket.count} X {price}원</div>
+                            <div className='selected-ticket-price'>{ticket.count * price}원</div>
+                        </div>
+                    );
+                })}
+
+                {/* total amount */}
+                {ticketData.some(ticket => ticket.count > 0) && (
+                    <div className='total-container'>
+                        <span className='total-title'>총 여행 금액</span>
+                        <span className='total-amount'>
+                            {ticketData.reduce((sum, ticket) => {
+                                const selectedPriceIndex = getSelectedPriceIndex(ticket, range);
+                                const price = selectedPriceIndex !== -1 ? ticket.priceList[selectedPriceIndex].price : 0;
+                                return sum + ticket.count * price;
+                            }, 0)}원
+                        </span>
+                    </div>
+                )}
             </div>
+            
+            {/* payment button */}
+            {ticketData.some(ticket => ticket.count > 0) && (
+                <div className='pay-btn-container'>
+                    <button className='pay-btn'>결제하기</button>
+                </div>
+            )}
         </div>
-
     );
 }
 
