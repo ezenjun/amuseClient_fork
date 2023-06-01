d861ad5e7bdbfe29274d41225735d241f8e18684
rnjs010 <kyuri4809@naver.com>
2023-06-01 16:08:05 +0900

    add ReservationModal

--- client/src/DetailPage/Detail/Component/ReservationBottom/ReservationBottom.tsx
+++ client/src/DetailPage/Detail/Component/ReservationBottom/ReservationBottom.tsx
@@ -1,17 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './ReservationBottom.scss';
+import ReservationModal from './ReservationModal/ReservationModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 
 function ReservationBottom() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className="reservation-bottom">
             <button className="wish-btn">
                 <FontAwesomeIcon icon={faHeart} className="heart-icon" />
             </button>
 
-            <button className="purchase-btn" >구매하기</button>
+            <button className="purchase-btn" onClick={openModal}>구매하기</button>
+            {isModalOpen && (
+                <ReservationModal onClose={closeModal} />
+            )}
         </div>
     );
 }
