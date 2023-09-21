168113772c0bba5bc1f40f7bfb67f9f2923e6204
ezenjun <ezenjun07@gmail.com>
2023-09-21 17:37:07 +0900

    [#72] feat : OrderPage 정렬 및 반응형 적용

---
+++ client/src/DetailPage/OrderPage/Components/OrderDetail/styles.ts
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const OrderDetailContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	border: 1px solid yellowgreen;
+	gap: 4.6875rem;
+`;
+
+export const DetailSectionContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1.5625rem;
+	border: 1px solid purple;
+`;
---
+++ client/src/DetailPage/OrderPage/Components/OrderForm/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const OrderInfoContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	height: 100%;
+	gap: 0.9375rem;
+	box-sizing: border-box;
+	border: 1px solid orange;
+`;
