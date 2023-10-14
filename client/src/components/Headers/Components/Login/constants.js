70cab001cadaf9e728418a3d34b33b50abf6dcfe
cocoa <jwv0323@naver.com>
2023-10-14 16:48:32 +0900

    feat: Header의 Login 컴포넌트 분리

---
+++ client/src/components/Headers/Components/Login/constants.js
@@ -0,0 +1,6 @@
+export const LOGIN = {
+  LOGIN: "로그인",
+  LOGOUT: "로그아웃",
+  JOIN: "회원가입",
+  USER: "님 😊",
+};
---
+++ client/src/components/Headers/Components/Login/style.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Login = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 32px;
+  gap: 40px;
+`;
+
+export const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  color: #9e9e9e;
+  text-align: right;
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.24px;
+`;
