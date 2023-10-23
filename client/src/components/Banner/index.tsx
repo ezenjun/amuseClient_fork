dfb026715c91164a211aa0c0dff020c424c1dda3
cocoa <jwv0323@naver.com>
2023-10-23 22:45:18 +0900

    feat: Banner 컴포넌트 생성

---
+++ client/src/components/Banner/index.tsx
@@ -0,0 +1,29 @@
+import { CategoryData } from "../../Interfaces/DataInterfaces";
+import Style from "../../pages/SubPage/SubPage.module.css";
+import ChildTitle from "../../pages/SubPage/SubtitleImgs/ChildTitle.jpg";
+import * as S from "./style";
+
+interface BannerProps {
+  categoryData: CategoryData | null;
+  categoryImg: string | undefined;
+  mainDescription: string | undefined;
+  subDescription: string | undefined;
+}
+
+function Banners(categoryData: BannerProps) {
+  return (
+    categoryData && (
+      <div className={Style["subTitleContainer"]}>
+        <S.Image
+          className={Style["mainPicture.image"]}
+          src={categoryData.categoryImg ? categoryData.categoryImg : ChildTitle}
+          alt="Title img"
+        />
+        <h2 className={Style["subTitle"]}>{categoryData.mainDescription}</h2>
+        <h3 className={Style["subContent"]}>{categoryData.subDescription}</h3>
+      </div>
+    )
+  );
+}
+
+export default Banners;
