66a9b83939d1aa8a9b04d9a53824340ff5f4543c
cocoa <jwv0323@naver.com>
2023-10-31 20:31:09 +0900

    refactor: 리뷰 디자인 수정 및 스타일드 컴포넌트 적용

---
+++ client/src/pages/DetailPage/Detail/Component/Title/TitleDetail/index.tsx
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faLocationDot,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
+import * as S from "./style";
+
+interface TitleDetailProps {
+  country: string;
+  city: string;
+  title: string;
+  rated: number;
+  review_count: number;
+}
+
+function TitleDetail({
+  country,
+  city,
+  title,
+  rated,
+  review_count,
+}: TitleDetailProps) {
+  return (
+    <>
+      <S.Location>
+        {country}
+        <FontAwesomeIcon icon={faChevronRight} />
+        <S.City>
+          <FontAwesomeIcon icon={faLocationDot} />
+          {city}
+        </S.City>
+      </S.Location>
+
+      <S.Title>{title}</S.Title>
+
+      <S.Review>
+        <S.Star>
+          <FontAwesomeIcon icon={faStar} />
+        </S.Star>
+        {rated}
+        <S.Count>({review_count})</S.Count>
+        <S.Right>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </S.Right>
+      </S.Review>
+    </>
+  );
+}
+
+export default TitleDetail;
---
+++ client/src/pages/DetailPage/Detail/Component/Title/TitleDetail/style.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Location = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 600;
+  color: #848c94;
+  gap: 12px;
+`;
+
+export const City = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: left;
+  margin: 20px 0;
+  font-size: 24px;
+  font-weight: 700;
+  color: #343a40;
+`;
+
+export const Review = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 600;
+  gap: 8px;
+`;
+
+export const Star = styled.div`
+  margin-bottom: 1px;
+  font-size: 10px;
+  color: #e6003d;
+`;
+
+export const Count = styled.div`
+  color: #848c94;
+`;
+
+export const Right = styled.div`
+  margin-top: 1.5px;
+  font-size: 10px;
+  color: #848c94;
+`;
