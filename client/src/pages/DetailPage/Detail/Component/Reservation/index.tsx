576654b19a24a6063cc0e48c48e2684d11fe0be9
cocoa <jwv0323@naver.com>
2023-11-01 13:46:49 +0900

    refactor: Reservation 상수 분리

--- client/src/pages/DetailPage/Detail/Component/Reservation/index.tsx
+++ client/src/pages/DetailPage/Detail/Component/Reservation/index.tsx
@@ -7,6 +7,7 @@ import Payment from "../../../../../components/Payment";
 import Share from "./share.svg";
 import Heart from "./wish.svg";
 import * as S from "./style";
+import * as C from "./constants";
 import axios from "axios";
 
 interface ReservationProps {
@@ -25,7 +26,7 @@ function Reservation({ itemId, productCode, likeNum }: ReservationProps) {
         setStartPrice(response.data.data.startPrice);
       })
       .catch((error) => {
-        console.log("연결 실패");
+        console.log("Reservation, startPrice 연결 실패");
       });
   }, [itemId]);
 
@@ -41,17 +42,15 @@ function Reservation({ itemId, productCode, likeNum }: ReservationProps) {
         setGuideData(response.data.data);
       })
       .catch((error) => {
-        console.log("연결 실패");
+        console.log("Reservation, Manager 연결 실패");
       });
   }, [itemId]);
 
   // Email Connect
   const handleInquiryClick = () => {
     if (guideData && guideData.email) {
-      const subject = encodeURIComponent(`[어뮤즈트래블 상품 문의하기]`);
-      const body = encodeURIComponent(
-        "안녕하세요, AmuseTravel 입니다 :-)\n\n아래에 문의 내용을 입력해주세요.\n\n감사합니다.\n\n---------------------\n\n"
-      );
+      const subject = encodeURIComponent(C.EMAIL.TITLE);
+      const body = encodeURIComponent(C.EMAIL.BODY);
       window.location.href = `mailto:${guideData.email}?subject=${subject}&body=${body}`;
     }
   };
@@ -67,10 +66,13 @@ function Reservation({ itemId, productCode, likeNum }: ReservationProps) {
       <S.Main>
         {startPrice ? (
           <>
-            <S.StartPrice>시작가</S.StartPrice>
+            <S.StartPrice>{C.RESERVATION.START_PRICE}</S.StartPrice>
             <S.Info>
               <S.Price>
-                <S.PriceNum>{startPrice.toLocaleString("en")}원</S.PriceNum>
+                <S.PriceNum>
+                  {startPrice.toLocaleString("en")}
+                  {C.RESERVATION.WON}
+                </S.PriceNum>
                 <S.PriceMin>~</S.PriceMin>
               </S.Price>
               <S.Share src={Share} onClick={handleTooltipToggle} />
@@ -78,18 +80,18 @@ function Reservation({ itemId, productCode, likeNum }: ReservationProps) {
           </>
         ) : (
           <S.Info>
-            <S.StartPrice>{"상품 준비중"}</S.StartPrice>
+            <S.StartPrice>{C.RESERVATION.PREPARE}</S.StartPrice>
             <S.Share src={Share} onClick={handleTooltipToggle} />
           </S.Info>
         )}
         <Payment />
         <S.Wish>
           <S.Heart src={Heart} />
-          위시리스트
+          {C.RESERVATION.WISH}
         </S.Wish>
         <S.HeartCount>
-          {likeNum === undefined ? 0 : likeNum}명이 이 상품을 위시리스트에
-          담았습니다
+          {likeNum === undefined ? 0 : likeNum}
+          {C.RESERVATION.WISH_NUMBER}
         </S.HeartCount>
       </S.Main>
 
@@ -108,12 +110,15 @@ function Reservation({ itemId, productCode, likeNum }: ReservationProps) {
             className="icon"
             icon={isHovered ? regularFaEnelope : solidFaEnelope}
           />
-          <S.InquiryText>문의하기</S.InquiryText>
+          <S.InquiryText>{C.RESERVATION.INQUIRY}</S.InquiryText>
         </S.Inquiry>
       </S.Manager>
 
       <S.ProductCode>
-        <S.ProductCodeText>상품코드 : {productCode}</S.ProductCodeText>
+        <S.ProductCodeText>
+          {C.RESERVATION.CODE}
+          {productCode}
+        </S.ProductCodeText>
       </S.ProductCode>
     </S.Reservation>
   );
