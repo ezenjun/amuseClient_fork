03cb6bb8f20654b60beaacb45e341956d6dacdf1
cocoa <jwv0323@naver.com>
2023-11-02 23:05:02 +0900

    style: Banner 컴포넌트 height값 계산

--- client/src/components/Banner/style.ts
+++ client/src/components/Banner/style.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Banner = styled.div`
-  min-width: 100%;
+  width: 100%;
+  height: 100%;
   padding: 0 10%;
 `;
 
@@ -9,9 +10,6 @@ export const Image = styled.img`
   object-fit: cover;
   cursor: pointer;
   width: 100%;
-  height: 140px;
-
-  @media (max-width: 768px) {
-    height: 100px;
-  }
+  max-height: 9.5vw;
+  min-height: 100px;
 `;
