7a8403cc5fd51f481bfa38f9a22ca8a95149ea26
cocoa <jwv0323@naver.com>
2023-11-02 23:12:57 +0900

    style: MainBanner 컴포넌트 height값 계산

--- client/src/components/MainBanner/style.ts
+++ client/src/components/MainBanner/style.ts
@@ -3,18 +3,6 @@ import styled from "styled-components";
 export const Image = styled.img`
   width: 100%;
   object-fit: cover;
-
-  @media (min-width: 1024px) {
-    /* Desktop styles */
-    height: 330px;
-  }
-  @media (min-width: 769px) and (max-width: 1023px) {
-    /* Tablet styles */
-    height: 192.5px;
-  }
-
-  @media (max-width: 768px) {
-    /* Mobile styles */
-    height: 184px;
-  }
+  max-height: 22.5vw;
+  min-height: 185px;
 `;
