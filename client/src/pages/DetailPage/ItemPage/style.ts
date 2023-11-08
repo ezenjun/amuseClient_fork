7f3ca1d91fdc39e9defb27219209dc17f9320c57
cocoa <jwv0323@naver.com>
2023-11-09 01:28:20 +0900

    feat: 1170px에서 Side 숨김 처리

--- client/src/pages/DetailPage/ItemPage/style.ts
+++ client/src/pages/DetailPage/ItemPage/style.ts
@@ -3,14 +3,18 @@ import styled from "styled-components";
 export const Detail = styled.div`
   display: flex;
   padding: 50px 10%;
-  justify-content: space-between;
   gap: 20px;
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100vw;
   gap: 35px;
 `;
 
-export const Side = styled.div``;
+export const Side = styled.div`
+  @media (max-width: 1170px) {
+    display: none;
+  }
+`;
