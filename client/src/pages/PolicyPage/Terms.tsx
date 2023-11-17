775742e97969b0bc6c797c051b9645b4d3106d21
rnjs010 <kyuri4809@naver.com>
2023-11-17 15:16:28 +0900

    개인정보 처리방침 페이지 추가, 소셜로그인 에러 수정

---
+++ client/src/pages/PolicyPage/Terms.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MainComponent from "../../MainComponent";
+
+
+const Terms: React.FC = () => {
+	const navigate = useNavigate();
+
+	return (
+		<MainComponent>
+            <div>
+				{/* 여기에 내용 작성될 예정 */}
+			</div>
+		</MainComponent>
+	);
+}
+
+export default Terms;
