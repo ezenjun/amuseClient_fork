1aadc2fc02c1ed77636c1f6e611c2987fcfbbe86
cocoa <jwv0323@naver.com>
2023-12-05 19:30:45 +0900

    feat: 위시리스트에 로그인 알림창 기능 추가

--- client/src/components/List/Box/index.tsx
+++ client/src/components/List/Box/index.tsx
@@ -22,25 +22,30 @@ function Box({ itemId, handleClick, title, startPrice, imageUrl }: BoxProps) {
     updatedIsLiked[itemId] = !updatedIsLiked[itemId];
     setIsLiked(updatedIsLiked);
     const token = cookies["__jwtkid__"];
-    axios
-      .post(
-        `${process.env.REACT_APP_AMUSE_API}/detail/${itemId}/like-plus`,
-        null,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `${token}`,
-          },
-        }
-      )
-      .then((response) => {
-        alert(
-          "관심상품에 등록되었습니다.\n관심상품 관리는 마이페이지에서 가능합니다."
-        );
-      })
-      .catch((error) => {
-        console.error("Box 위시리스트 에러", error);
-      });
+    if (token) {
+      axios
+        .post(
+          `${process.env.REACT_APP_AMUSE_API}/detail/${itemId}/like-plus`,
+          null,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          alert(
+            "관심상품에 등록되었습니다.\n관심상품 관리는 마이페이지에서 가능합니다."
+          );
+        })
+        .catch((error) => {
+          console.error("Box 위시리스트 에러", error);
+        });
+    }
+    if (token === undefined) {
+      alert("로그인이 필요합니다.");
+    }
   };
 
   return (
